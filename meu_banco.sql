9a668ac8985539515ef089ee5169d89422448d45
Alexandre Vieira <99272946+ALEavdsz@users.noreply.github.com>
2022-03-16 13:45:32 -0300

    Add files via upload

---
+++ meu_banco.sql
@@ -0,0 +1,18 @@
+create table tb_alunos(nome varchar(100), idade integer);
+
+create table tb_professores(nome varchar(100), idade integer);
+
+create table tb_disciplinas(nome varchar(100), vagas integer);
+
+create table tb_departamentos(nome varchar(100), funcionarios integer);
+
+create table tb_laboratorios(nome varchar(100), n_sala integer);
+
+create table tb_funcionarios(nome varchar(100));
+
+create table tb_(nome varchar(100), vagas integer);
+
+create table tb_reprovados(nome varchar(100), disciplina varchar(100), nota integer);
+
+create table tb_estagiarios(nome varchar(100), idade integer);
+
